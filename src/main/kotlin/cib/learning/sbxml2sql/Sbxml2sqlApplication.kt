2d8be39785c34ef23947b4704ff505410355f0aa
Roman Dyachkov <niarbrnd@yandex.ru>
2022-06-24 06:23:17 +0300

    add Arguments.kt

--- src/main/kotlin/cib/learning/sbxml2sql/Sbxml2sqlApplication.kt
+++ src/main/kotlin/cib/learning/sbxml2sql/Sbxml2sqlApplication.kt
@@ -1,5 +1,6 @@
 package cib.learning.sbxml2sql
 
+import cib.learning.sbxml2sql.Service.Arguments
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,4 +9,12 @@ class Sbxml2sqlApplication
 
 fun main(args: Array<String>) {
     runApplication<Sbxml2sqlApplication>(*args)
+    Arguments().printHello()
+
+    /*  System.out.println("Start app");
+        for(int i = 0; i<args.length; i++) {
+            System.out.println("args[" + i + "]: " + args[i]);
+        }*/
+    val options = Arguments().gets(args)
+    //System.out.println(options.toString())
 }
